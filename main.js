e9f8a2fecbda6a1b2c41fe4c7dc5ec9c02c5fe19
spreyo <60149407+spreyo@users.noreply.github.com>
2022-08-24 12:51:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const { TeamSpeak, QueryProtocol } = require('ts3-nodejs-library');
+
+TeamSpeak.connect({
+    host: "ts3.spreyo.tk",
+    protocol: QueryProtocol.RAW,
+    serverport: 9987,
+    username: "serveradmin",
+    password: "15231306",
+    nickname: "NodeJS Bot"
+}).then(async teamspeak =>{ 
+    const clients = await teamspeak.clientList({ clientType: 0})
+    clients.forEach(client =>{
+        console.log("sending hello message to ", client.nickname)
+        client.message('Hello');
+    })
+}).catch(e=>{
+    console.log('error');
+    console.error(e)
+})
